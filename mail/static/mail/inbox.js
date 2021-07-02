bd3f9a5361bb601fd396b99a6f3fdbdb46b90554
Thomas Newton <tomnewtonx@gmail.com>
2021-07-02 17:03:03 +0100

    Render tables dynamically based on inbox

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -15,9 +15,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function compose_email() {
 
-  // Show compose view and hide other views
-  document.querySelector('#emails-view').style.display = 'none'; // Hides the emails view HTML div. 
-  document.querySelector('#compose-view').style.display = 'block'; //shows block of the compose email view div html. 
+    // Show the mailbox and hide other views
+    document.querySelector('#emails-view').style.display = 'none'; //hides the emails-view div (inbox titlename). 
+    document.querySelector('#compose-view').style.display = 'block'; // Shows block where you compose email. 
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -45,28 +45,72 @@ const send_email = (ev) => {
 
   document.querySelector('#compose-form').reset(); //Clears the form for the next entry.
 
-  load_mailbox('sent') // Loads the users sent mailbox
+  load_mailbox('sent') // Loads the users sent mailbox by executing the load_mailbox function
 
 }
 
 function load_mailbox(mailbox) {
   
-  // Show the mailbox and hide other views
-  document.querySelector('#emails-view').style.display = 'block'; //shows block of the emails view id HTML div. 
-  document.querySelector('#compose-view').style.display = 'none'; // Hides the compose view HTML div. 
-
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
+  // Show and hide neccessary views.
+  document.querySelector('#compose-view').style.display = 'none'; // Hides the compose view HTML div.
+  document.querySelector('#email-table').style.display = 'block'; // Hides the compose view HTML div.
+
   // Gets all the emails in the given mailbox
+
+
   fetch(`/emails/${mailbox}`, {
     method: 'GET',
   })
   .then(response => response.json())
   .then(result => {
+    // Build email row item
+    buildTable(result)
     // Print result
     console.log(result);
   });
+
+
+  function buildTable(data){
+    var email_item = document.getElementById('emailTable')
+    email_item.innerHTML = ''
+
+    for (var i = 0; i < data.length; i++){
+      if(mailbox == 'inbox' || 'archive'){
+        if(data.read = 'true'){
+      var row = `<tr class="read">
+                  <td class="action"><input type="checkbox" /></td>
+                  <td class="action"><i class="fa fa-star-o"></i></td>
+                  <td class="name" <a href="#">${data[i].recipients}</td>
+                  <td class="subject" <a href="#">${data[i].subject}</td>
+                  <td class="time" <a href="#">${data[i].timestamp}</td>
+                </tr>`
+      email_item.innerHTML += row}
+          else {
+            var row = `<tr class="read" >
+                  <td class="action"><input type="checkbox" /></td>
+                  <td class="action"><i class="fa fa-star-o"></i></td>
+                  <td class="name" <a href="#">${data[i].recipients}</td>
+                  <td class="subject" <a href="#">${data[i].subject}</td>
+                  <td class="time" <a href="#">${data[i].timestamp}</td>
+                </tr>`
+          }
+      } else if (mailbox == 'sent'){
+        var row = `<tr>
+                  <td class="action"><input type="checkbox" /></td>
+                  <td class="action"><i class="fa fa-star-o"></i></td>
+                  <td class="name" <a href="#">${data[i].sender}</td>
+                  <td class="subject" <a href="#">${data[i].subject}</td>
+                  <td class="time" <a href="#">${data[i].timestamp}</td>
+                </tr>`
+      email_item.innerHTML += row
+      }
+
+      }
+    }
+
 
 }
 
